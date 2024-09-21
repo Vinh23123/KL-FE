c95b27de43f6042e9022b28204a48debb7695831
Vinh <vinhhoangvh11@gmail.com>
2024-09-21 21:46:05 +0700

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Route } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Route path="/home" Component={} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/AppLayout.jsx
@@ -0,0 +1,3 @@
+const AppLayout = () => {
+  return <dir>hi</dir>;
+};
